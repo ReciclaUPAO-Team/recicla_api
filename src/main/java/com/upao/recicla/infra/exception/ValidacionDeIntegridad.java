efe8f34eac8bcff7b94ac461506530d5929682c9
ralfaroh1 <ralfaroh1@upao.edu.pe>
2023-11-11 08:31:47 -0500

    Crear Recompensa

---
+++ src/main/java/com/upao/recicla/infra/exception/ValidacionDeIntegridad.java
@@ -0,0 +1,7 @@
+package com.upao.recicla.infra.exception;
+
+public class ValidacionDeIntegridad extends RuntimeException {
+      public ValidacionDeIntegridad(String s) {
+        super(s);
+      }
+}
