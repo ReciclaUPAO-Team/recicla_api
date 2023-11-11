920217434d581c7af6f9aa84773d22029da39eaa
ralfaroh1 <145864506+ralfaroh1@users.noreply.github.com>
2023-11-11 08:37:35 -0500

    Delete src/main/java/com/upao/recicla/domain/repository/ActividadRepository.java

--- src/main/java/com/upao/recicla/domain/repository/ActividadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upao.recicla.domain.repository;
-
-import com.upao.recicla.domain.entity.actividad.Actividad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActividadRepository extends JpaRepository<Actividad, Long> {
-}
