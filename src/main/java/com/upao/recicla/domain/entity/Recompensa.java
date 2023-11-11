5a16837037957d40f89512423df652708cd9855e
AnaPeredaOrtiz <cristiortiz1301@gmail.com>
2023-11-11 01:20:06 -0500

    Catalogo de recompensas

---
+++ src/main/java/com/upao/recicla/domain/entity/Recompensa.java
@@ -0,0 +1,51 @@
+package com.upao.recicla.domain.entity;
+
+import com.upao.recicla.domain.dto.recompensaDto.DatosActualizarRecompensa;
+import com.upao.recicla.domain.dto.recompensaDto.DatosRegistroRecompensa;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Table(name = "recompensas")
+@Entity(name = "Recompensa")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Recompensa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String categoria;
+    @Column(name = "puntos_requeridos")
+    private Double valor;
+    @Column(name = "fecha_inicio", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false)
+    private LocalDateTime fechaInicio;
+    @Column(name = "fecha_cierre")
+    private LocalDateTime fechaCierre;
+
+
+
+    public Recompensa(DatosRegistroRecompensa datos){
+        this.titulo = datos.titulo();
+        this.descripcion = datos.descripcion();
+        this.categoria = datos.categoria();
+        this.valor = datos.valor();
+    }
+
+    public void actualizarRecompensa(DatosActualizarRecompensa datosActualizarRecompensa){
+        if (datosActualizarRecompensa.titulo() != null){
+            this.titulo = datosActualizarRecompensa.titulo();
+        }
+        if(datosActualizarRecompensa.categoria() != null){
+            this.categoria = datosActualizarRecompensa.categoria();
+        }
+        if(datosActualizarRecompensa.valor() != null){
+            this.valor = datosActualizarRecompensa.valor();
+        }
+    }
+}
